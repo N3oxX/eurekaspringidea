fca2275b533f0d46db806349f81635698f3cf553
David Martinez <david.martinez@nter.es>
2020-03-09 16:00:29 +0100

    Initial commit

---
+++ src/test/java/com/mtzperez/eurekaspringidea/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtzperez.eurekaspringidea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
